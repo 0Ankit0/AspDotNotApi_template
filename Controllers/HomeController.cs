f6f638958a3b2861dabf44b8efc3b7e9fbe4af59
BenevolentButcher <ankit@insoftnepal.com>
2024-03-19 07:16:20 +0545

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceApp_backend.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Welcome to the home page");
+        }
+    }
+}
---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceApp_backend.Models
+{
+    public class ResponseModel
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public object data { get; set; }
+    }
+}
