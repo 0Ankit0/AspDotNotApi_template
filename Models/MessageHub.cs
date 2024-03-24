79e50d33eaedd7bbd96deaa7e0ca603fe862b10f
BenevolentButcher <ankit@insoftnepal.com>
2024-03-24 07:01:35 +0545

    added signalr library

---
+++ Models/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServiceApp_backend.Models
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            if (string.IsNullOrEmpty(user))
+                await Clients.All.SendAsync("ReceiveMessageHandler", message);
+            else
+                await Clients.User(user).SendAsync("ReceiveMessageHandler", message);
+        }
+    }
+}
+
+
